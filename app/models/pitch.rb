36faaad5dc4ae3605dda2b1b27f1ed77ee47dec8
Elisa Verna & Ephraim Friedman & parker smathers & Kiren Srinivasan <pair+elisuh.ephraimfriedman.psmathers.tuneguru@devbootcamp.com>
2016-09-19 15:33:45 -0400

    Adds team_id field on pitches table and model

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -1,5 +1,6 @@
 class Pitch < ApplicationRecord
   validates :pitcher_id,  { presence: true }
+  validates :team_id,     { presence: true }
   validates :title,       { presence: true }
   validates :summary,     { presence: true }
 
--- db/migrate/20160919191814_create_pitches.rb
+++ db/migrate/20160919191814_create_pitches.rb
@@ -2,6 +2,7 @@ class CreatePitches < ActiveRecord::Migration[5.0]
   def change
     create_table :pitches do |t|
       t.integer     :pitcher_id,    { null: false }
+      t.integer     :team_id,       { null: false }
       t.string      :title,         { null: false }
       t.text        :summary,       { null: false }
       t.timestamps(null: false)
