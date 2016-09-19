1a84b4b2aee08c13a0110d9a2995e54d0e5f57ce
Elisa Verna & Ephraim Friedman & parker smathers & Kiren Srinivasan <pair+elisuh.ephraimfriedman.psmathers.tuneguru@devbootcamp.com>
2016-09-19 18:20:56 -0400

    Adds initial cohort configuration form

--- app/assets/javascripts/components/AdminDashboard.es6.jsx
+++ app/assets/javascripts/components/AdminDashboard.es6.jsx
@@ -2,52 +2,23 @@ class AdminDashboard extends React.Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.addNewStudent = this.addNewStudent.bind(this)
-    this.state = {
-      students: []
-    }
   }
 
   handleSubmit(event) {
 
   }
 
-  addNewStudent(event) {
-    event.preventDefault();
-
-    var username = this.refs.username.value;
-    var email = this.refs.email.value;
-
-    this.setState({
-      students: this.state.students.concat({username, email})
-    })
-
-    $.ajax({
-      url: "/students",
-      method: "POST",
-      data: {
-        username: this.state.students.slice(-1)[0].username,
-        email: this.state.students.slice(-1)[0].email
-      }
-    })
-  }
-
   render() {
     return(
       <form onSubmit={this.handleSubmit}>
         <input placeholder="DBC Cohort Name" type="text" /><br></br>
+
         <label for="maxPitches">Max # of Pitches per Student:</label>
         <input type="number" name="maxPitches" max="5" /><br></br>
-        {
-          this.state.students.map((student) =>
-            <Student student={student} />
-          )
-        }
-        <form onSubmit={this.addNewStudent}>
-          <input placeholder="Student Username" ref="username" type="text" />
-          <input placeholder="Student Email" ref="email" type="text" />
-          <input type="submit" value="+" />
-        </form>
+
+        <label for="roundVotes">How many pitches can a student vote for in both of the rounds?</label><br></br>
+        <input type="number" name="roundVotes" /><br></br>
+
         <input type="submit" value="Create New Cohort" />
       </form>
     )
---
+++ app/assets/javascripts/components/DashboardContent.es6.jsx
@@ -0,0 +1,1 @@
+// Refer to complete-intro-to-react to use the same technique used to pass different props to "Header" from "Search" and "Details"
