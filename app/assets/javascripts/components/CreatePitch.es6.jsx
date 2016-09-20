725abd4c6eb8d28a448855605e7b07f67470c930
psmathers <parkersmathers@yahoo.com>
2016-09-20 07:20:50 -0400

    Add pitches controller and create-pitch component

---
+++ app/assets/javascripts/components/CreatePitch.es6.jsx
@@ -0,0 +1,8 @@
+class CreatePitch extends React.Component {
+
+  render() {
+    return(
+      
+    )
+  }
+}
---
+++ app/controllers/pitches_controller.rb
@@ -0,0 +1,4 @@
+class PitchesController < ApplicationController
+  def index
+  end
+end
